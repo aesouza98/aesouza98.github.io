1d86614a4f53fedf27f76948553f5fd35e9ceb70
Adriano Elias <github@nano.slmail.me>
2026-07-26 00:04:23 -0300

    Quartz sync: Jul 26, 2026, 12:04 AM

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -71,7 +71,7 @@ export interface GlobalConfiguration {
    *   Quartz will avoid using this as much as possible and use relative URLs most of the time
    */
   baseUrl?: string
-  theme: Theme
+  theme: default
   /**
    * Allow to translate the date in the language of your choice.
    * Also used for UI translation (default: en-US)
